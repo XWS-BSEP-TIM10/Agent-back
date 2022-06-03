c02db066f19d998ce8b0f77408af67ca84827ecf
Kina999 <katarinazer6@gmail.com>
2022-06-03 21:33:29 +0200

    fix: position in salary dto

--- Agent/src/main/java/com/agent/dto/GetSalaryDTO.java
+++ Agent/src/main/java/com/agent/dto/GetSalaryDTO.java
@@ -5,14 +5,20 @@ import com.agent.model.Salary;
 public class GetSalaryDTO {
     private String id;
     private double value;
+    private String position;
 
     public GetSalaryDTO(Salary salary) {
         this.id = salary.getId();
+        this.position = salary.getPosition();
         this.value = salary.getValue();
     }
 
     public String getId() {
         return id;
+    }
+
+    public String getPosition() {
+        return position;
     }
 
     public double getValue() {
