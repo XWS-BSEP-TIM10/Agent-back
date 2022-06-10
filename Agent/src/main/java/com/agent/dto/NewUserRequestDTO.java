08fa488ba1b87220a97858088625342b69fd1a02
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2022-06-10 17:33:55 +0200

    feat: xss and validation added

--- Agent/src/main/java/com/agent/dto/NewUserRequestDTO.java
+++ Agent/src/main/java/com/agent/dto/NewUserRequestDTO.java
@@ -1,16 +1,28 @@
 package com.agent.dto;
 
+import com.agent.validator.BlackList;
+import com.agent.validator.FieldMatch;
+import com.agent.validator.PasswordContainsEmail;
+import com.agent.validator.ValidPassword;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+@FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match")
+@PasswordContainsEmail(password = "password", email = "email", message = "The password can not contain email")
 public class NewUserRequestDTO {
 
     @NotBlank
-    @Email
+    @Email(message = "Email is not valid")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password is mandatory")
+    @ValidPassword
+    @BlackList
     private String password;
+
+    @NotBlank(message = "Confirm password is mandatory")
+    private String confirmPassword;
 
     public NewUserRequestDTO(String email, String password) {
         this.email = email;
@@ -26,5 +38,9 @@ public class NewUserRequestDTO {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
     }
 }
